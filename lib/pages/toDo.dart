5c675f8c444d09dcc5d8b92587d445200a559e31
Afsna Akter <tarinakter567@gmail.com>
2025-02-18 23:37:14 +0600

    make UI design and funtionality

--- lib/pages/toDo.dart
+++ lib/pages/toDo.dart
@@ -8,6 +8,55 @@ class ToDo extends StatefulWidget {
 }
 
 class _ToDoState extends State<ToDo> {
+
+  List<Map<String, dynamic>> tasks =[];
+  bool showActiveTask = true;
+
+  void showTaskDialog({int? index}){
+    TextEditingController _taskController = TextEditingController();
+    showDialog(context: context, builder: (context){
+      return AlertDialog(
+        title: Text('Add Task'),
+        content: TextField(
+          controller: _taskController,
+          decoration: InputDecoration(
+            hintText: 'Enter Your task',
+            
+          ),
+          
+        ),
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.pop(context);
+          }, child: Text('Cancel')),
+          TextButton(onPressed: ()=>_addTask(_taskController.text), child: Text('Add'))
+        ],
+      );
+    });
+  }
+
+  void _addTask(String task){
+    setState(() {
+      tasks.add({
+        'task':task,
+        'completed':false,
+        });
+    });
+    Navigator.pop(context);
+  }
+
+  void _toggleTaskStatus(int index){
+    setState(() {
+      tasks[index]['completed'] = !tasks[index]['completed'];
+    });
+  }
+
+  void _deleteTask(int index){
+    setState(() {
+      tasks.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,19 +73,109 @@ class _ToDoState extends State<ToDo> {
       
       body:Column(
         children: [
-          Row(children: [
-            Container(
-
-            )
-          ],)
+          SizedBox(height: 20,),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.black,width: 2),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blue,
+                      offset: Offset(0, 3),
+                      blurRadius: 5
+                    )
+                  ]
+                ),
+                child: Row(children: [
+                  Column(children: [
+                    Text("Active", style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500
+                    )),
+                    Text('10',style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500
+                    ))
+                  ],)
+                ]),
+            
+              ),
+               Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.black,width: 2),
+                   boxShadow: [
+                    BoxShadow(
+                      color: Colors.blue,
+                      offset: Offset(0, 3),
+                      blurRadius: 5
+                    )
+                  ]
+                ),
+                child: Row(children: [
+                  Column(children: [
+                    Text("Completed", style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500
+                    )),
+                    Text('10',style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500
+                    ))
+                  ],)
+                ]),
+            
+              )
+            ],),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index){
+                return Dismissible(
+                  key:Key(tasks[index]['task']),
+                  background: Container(
+                    color: Colors.green,
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(left: 20),
+                    child: Icon(Icons.delete, color: Colors.white,),
+                  ),
+                  secondaryBackground: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(right: 20),
+                    child: Icon(Icons.check, color: Colors.white,),
+                  ),
+                  onDismissed:(direction){
+                    if(direction == DismissDirection.startToEnd){
+                      _toggleTaskStatus(index);
+                    }else{
+                      _deleteTask(index);
+                    }
+                  } ,
+                  child: Card(
+                    child: ListTile(
+                      title: Text(tasks[index]['task']),
+                    ),
+                  ),
+                );
+            }),
+          )
         ],
       ) ,
 
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue.shade400,
-        onPressed: (){
-
-        },
+        onPressed: ()=>showTaskDialog(),
         child: Icon(Icons.add, color: Colors.white,),),
     );
   }
